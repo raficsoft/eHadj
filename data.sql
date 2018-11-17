dc2e15d2b420a3bac12831680bde4d117f08deb1
Ibrahim Guebre <guebbou@hotmail.com>
2018-11-17 16:07:50 +0000

    GUEBRE: CREATION DE LA TABLE ADMIN_SYST DE data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE monpelerinage_db;
+
+/** Creation d'une table  des admins **/
+CREATE TABLE admin_syst(
+    id_user INT(6),
+    login_user VARCHAR(60) NOT NULL,
+    password_user VARCHAR(60) NOT NULL,
+    name_user VARCHAR(45),
+    prenom_user VARCHAR(60),
+    login_date DATETIME,
+    logout_date DATETIME , 
+    CONSTRAINT pk_user PRIMARY KEY(id_user)
+)ENGINE=InnoDB;
